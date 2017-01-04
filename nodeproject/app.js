35d4461284ce5b1e13e832638b79007fab079c2b
Chad <zxponent@gmail.com>
2017-01-04 19:46:22 +0000

    bootstrap added as app.js

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,18 @@
-var aloha = 'Aloha Var!';
-console.log(aloha);
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT; //5000 normaly
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.get('/', function(req, res){
+    res.send('Digitalsos World!');
+});
+
+app.get('/routing', function(req, res){
+    res.send('Digitalsos routing!');
+});
+
+app.listen(port, function(err){
+   console.log('The server is running on port: ' + port);
+});
